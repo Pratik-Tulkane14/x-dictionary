3af58766f13de6c39c0795225b6ba51a76affb3d
Pratik-Tulkane14 <pratiktulkane12345@gmail.com>
2025-03-24 10:16:42 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [input, setInput] = useState("");
+  const [output, setOutput] = useState("");
+const data = [
+  {
+    word: "React",
+    meaning: "A JavaScript library for building user interfaces.",
+  },
+
+  { word: "Component", meaning: "A reusable building block in React." },
+
+  { word: "State", meaning: "An object that stores data for a component." },
+];
+const handleSubmit=(e)=>{
+  e.preventDefault();
+  const result = data.filter((item)=>{
+    if(item.word.toLowerCase() ===input.toLowerCase()){
+      return item
+    }
+  });
+  setOutput(result[0]?.meaning ||"Word not found in the dictionary.");
+}
+  return (
+    <>
+      <h1>Dictionary App</h1>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input
+          type="text"
+          required
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+      <h3>Definition:</h3> <p>{output}</p>
+    </>
+  );
+}
+
+export default App
